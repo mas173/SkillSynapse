61de4aef6b957a7720ef9d0ac8133aab3c908d40
Vicky Raj <rajv60701@gmail.com>
2026-02-15 13:52:14 +0530

    login features implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { emailSignup } from "../controllers/auth/emailsignUp.js";
+import { emailLogin } from "../controllers/auth/emailLogin.js";
 import { googleSignup } from "../controllers/auth/googleSignUp.js";
 
 const Authrouter = express.Router();
 Authrouter.post("/emailPass", emailSignup);
+Authrouter.post("/emailLogin", emailLogin);
 Authrouter.post("/google", googleSignup);
 
 export default Authrouter;
